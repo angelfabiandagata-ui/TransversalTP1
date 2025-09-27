42780d64b4e5156fe9334c7f0fd359a48169fb88
Nico-ceo <teito.bris@gmail.com>
2025-09-27 16:22:18 -0300

    AlumnoDatos
    agregue en la carpeta Persistencia datos del alumno

---
+++ TransversalTP1-grupo8/src/Persistencia/AlumnoDatos.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencia;
+
+import java.time.LocalDate;
+
+//Atributos alumnodatos
+public class AlumnoDatos {
+     private int idAlumno;//int
+     private int dni;//int
+     private String apellido;//varchar
+     private String nombre;//varchar
+     private LocalDate fechadenacimiento;//date
+     private boolean estado;//tinyint
+
+    public AlumnoDatos(int idAlumno, int dni, String apellido, String nombre, LocalDate fechadenacimiento, boolean estado) {
+        this.idAlumno = idAlumno;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechadenacimiento = fechadenacimiento;
+        this.estado = estado;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+
+    public void setFechadenacimiento(LocalDate fechadenacimiento) {
+        this.fechadenacimiento = fechadenacimiento;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "-AlumnoDatos- " + " idAlumno: " + idAlumno + ", dni: " + dni + ", apellido: " + apellido + ", nombre: " + nombre + ", fechadenacimiento: " + fechadenacimiento + ", estado: " + estado;
+    }
+    
+}
