3d5cdb844e60135ac2abac762c3b60adab403cf8
Lucas <serranolucas1000@gmail.com>
2025-09-27 16:22:13 -0300

    Creacion clase Alumno en paquete modelo

--- TransversalTP1-grupo8/src/Modelo/Conexion.java
+++ TransversalTP1-grupo8/src/Modelo/Conexion.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author angel
- */
+import com.sun.jdi.connect.spi.Connection;
+import org.mariadb.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
-    
+
+    private String url;
+    private String usuario;
+    private String password;
+
+    private static Connection conexion = null;
+
+    public Conexion(String url, String usuario, String password) {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public Connection buscarconexion() {
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = (Connection) DriverManager.getConnection(url, usuario, password);
+
+            } catch (SQLException | ClassNotFoundException ex) {
+                System.out.println("No se puede conectar o no se puedo cargar el driver ");
+            }
+        }
+        return conexion;
+    }
+
 }
---
+++ TransversalTP1-grupo8/src/Modelo/alumno.java
@@ -0,0 +1,55 @@
+
+package Modelo;
+
+import java.time.LocalDate;
+
+
+public class alumno {
+    private int id = -1; 
+    private String nombre; //Varchar
+    private LocalDate fechaNac; // datetime
+    private boolean activo; //tinyint
+
+    public alumno(String nombre, LocalDate fechaNac, boolean activo) {
+        this.id = -1;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
+    
+    
+}
