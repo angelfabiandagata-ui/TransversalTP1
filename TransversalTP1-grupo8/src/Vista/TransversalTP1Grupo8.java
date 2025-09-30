c150726347d9dd0f3954fc1537e0524a1d09ff35
Lucas <serranolucas1000@gmail.com>
2025-09-30 19:04:55 -0300

    se aplica un format y comentarios

--- TransversalTP1-grupo8/src/Vista/TransversalTP1Grupo8.java
+++ TransversalTP1-grupo8/src/Vista/TransversalTP1Grupo8.java
@@ -13,22 +13,27 @@ public class TransversalTP1Grupo8 {
         Conexion c = new Conexion("jdbc:mariadb://localhost:3306/sgulp_equipo_8", "root", "");
         Connection con = c.buscarConexion();
         AlumnoDatos al = new AlumnoDatos(con);
-        Alumno alm = new Alumno(47267078, "Serrano", "Lucas", LocalDate.of(2006, Month.MARCH, 31), true);
-        Alumno alm1 = new Alumno(36881187, "D'Agata", "Fabian", LocalDate.of(1991, Month.NOVEMBER, 29), true);
-        Alumno alm2 = new Alumno(45886408, "Amaya", "Leandro", LocalDate.of(2004, Month.SEPTEMBER, 21), true);
-        Alumno alm3 = new Alumno(37296838, "Sosa", "Daiana", LocalDate.of(1993, Month.MAY, 31), true);
-        Alumno alm4 = new Alumno(44953034, "Miranda", "Teo", LocalDate.of(2003, Month.JUNE, 30), true);
+        
+        //CREACION DE ALUMNOS
+        Alumno alumno1 = new Alumno(47267078, "Serrano", "Lucas", LocalDate.of(2006, Month.MARCH, 31), true);
+        
+        Alumno alumno2 = new Alumno(36881187, "D'Agata", "Fabian", LocalDate.of(1991, Month.NOVEMBER, 29), true);
+        
+        Alumno alumno3 = new Alumno(45886408, "Amaya", "Leandro", LocalDate.of(2004, Month.SEPTEMBER, 21), true);
+        
+        Alumno alumno4 = new Alumno(37296838, "Sosa", "Daiana", LocalDate.of(1993, Month.MAY, 31), true);
+        
+        Alumno alumno5 = new Alumno(44953034, "Miranda", "Teo", LocalDate.of(2003, Month.JUNE, 30), true);
+
+
+//GUARDAR ALUMNO
+
 //        al.guardarAlumno(alm);
-//        al.guardarAlumno(alm1);
-//        al.guardarAlumno(alm2);
-//        al.guardarAlumno(alm3);
-//        al.guardarAlumno(alm4);
-        //Prueba borrar alumno
-        al.borrarAlumno(16);
-        al.borrarAlumno(17);
-        al.borrarAlumno(18);
-        al.borrarAlumno(19);
-        al.borrarAlumno(20);
+//BORRAR ALUMNO
+
+        //al.borrarAlumno();
+//LISTAR ALUMNOS
+
         //al.listarAlumnos();
     }
 
