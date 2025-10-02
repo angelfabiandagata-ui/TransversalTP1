ac48aa7ca30acd3ef20b4a7f85b5574eeb1717bc
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-10-02 18:30:42 -0300

    actualizacion materia
    se le agregaron atributos, getters y setters

--- TransversalTP1-grupo8/src/Modelo/Materia.java
+++ TransversalTP1-grupo8/src/Modelo/Materia.java
@@ -10,4 +10,53 @@ package Modelo;
  */
 public class Materia {
     
+    //atributos
+    private int idmateria;
+    private String nombre;
+    private String anio;   
+    private boolean estado;
+
+    //constructor completo
+    public Materia(int idmateria, String nombre, String anio, boolean estado) {
+        this.idmateria = idmateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+   
+   
+    //getters y setters
+    public int getIdmateria() {
+        return idmateria;
+    }
+
+    public void setIdmateria(int idmateria) {
+        this.idmateria = idmateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+//final    
 }
