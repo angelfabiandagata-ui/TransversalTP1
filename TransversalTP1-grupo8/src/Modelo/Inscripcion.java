54aade76acb16849253a61019e6d5093127deb33
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-10-07 19:31:05 -0300

    class inscripcion en modelo
    se crea clases con atributos y metodos

---
+++ TransversalTP1-grupo8/src/Modelo/Inscripcion.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Ema
+ */
+public class Inscripcion {
+    
+    //atributos
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    //constructor vacio
+    public Inscripcion() {
+    }
+
+    //con. todos los atributos
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    //con. todos los atributos menos id
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    //getters y setters
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+    
+    
+    
+    //tostrings
+    @Override
+    public String toString() {
+        return ("info: " + this.getIdInscripcion()+ " "+ this.getAlumno()+ " " +this.getMateria() + " " +this.getNota());
+    }
+    
+    
+}
