a835d4e67475cb3007aaeff3a6d1031a8ac73ae5
Daiana3105 <daianasosacarranza@gmail.com>
2025-10-17 16:00:38 -0300

    botonSalir

--- TransversalTP1-grupo8/src/Vista/ListadeAlumnosporMateria.java
+++ TransversalTP1-grupo8/src/Vista/ListadeAlumnosporMateria.java
@@ -93,6 +93,11 @@ private void borrarFilasTabla() {
         jScrollPane1.setViewportView(jTable1);
 
         jButton1.setText("Salir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setFont(new java.awt.Font("Verdana", 1, 18)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -146,6 +151,10 @@ private void borrarFilasTabla() {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
